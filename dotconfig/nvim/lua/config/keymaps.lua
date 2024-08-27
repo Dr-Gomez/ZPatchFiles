de279ce190e6c1a7186af854899cc97282797f85
Dr-Gomez <gomez.contact.mgz@gmail.com>
2024-08-27 16:37:06 +0100

    adds keybindings to navigation

--- dotconfig/nvim/lua/config/keymaps.lua
+++ dotconfig/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap
+
+-- j/k = down/up, h/l = left/right
+
+keymap.set("n", "<leader><Left>", "<C-w>h", opts)
+keymap.set("n", "<leader><Right>", "<C-w>l", opts)
