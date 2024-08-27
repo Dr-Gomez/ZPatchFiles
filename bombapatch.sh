6bbb738eafff4c6b060b936ea246d9ada46fd3b4
Marco Antonio Gomez <139799284+Dr-Gomez@users.noreply.github.com>
2024-08-27 11:59:28 +0100

    Updated basic packages.

--- bombapatch.sh
+++ bombapatch.sh
@@ -72,13 +72,51 @@ link_dotfiles() {
   done
 }
 
-install_packages() {
-  print_message "YELLOW" "INSTALLING BASIC PACKAGES ..."
+install_basic_packages() {
+  echo
+  msg="INSTALLING BASIC PACKAGES ..."
+  mkdir -p "${BINDIR}"
+  print_yellow "${msg}"
+  # sudo killall packagekitd
   sudo systemctl daemon-reload
   sudo add-apt-repository --yes multiverse
-  sudo apt update
-  sudo apt install -y plocate build-essential llvm pkg-config cmake ninja-build \
-    python3-pip curl git zsh tmux htop neofetch ripgrep fzf jq gettext
+  sudo aptitude update
+  sudo aptitude -y install plocate build-essential llvm \
+    pkg-config autoconf automake cmake cmake-data autopoint \
+    ninja-build gettext libtool libtool-bin g++ meson \
+    clang clang-tools ca-certificates curl gnupg lsb-release \
+    python-is-python3 ipython3 python3-pip python3-dev gawk \
+    unzip lzma tree neofetch git git-lfs zsh tmux gnome-tweaks \
+    inxi most ttfautohint v4l2loopback-dkms ffmpeg htop bc fzf \
+    ranger libxext-dev ripgrep python3-pynvim xclip libnotify-bin \
+    libfontconfig1-dev libfreetype-dev jq pixz hashdeep liblxc-dev \
+    libxrandr-dev libxinerama-dev libxcursor-dev libglx-dev libgl-dev \
+    screenkey mypaint rofi gimp blender imagemagick net-tools \
+    libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev \
+    libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \
+    libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+    libxcb-shape0-dev libxcb-xrm-dev libxcb-xrm0 libxcb-xkb-dev \
+    libconfig-dev libdbus-1-dev libegl-dev libpcre2-dev libpixman-1-dev \
+    libx11-xcb-dev libxcb-composite0-dev libxcb-damage0-dev \
+    libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev \
+    libxcb-render0-dev libxcb-render-util0-dev libxcb-util-dev \
+    libxcb-xfixes0-dev uthash-dev libxkbcommon-dev libxkbcommon-x11-dev \
+    xutils-dev asciidoc libconfuse-dev libasound2-dev libiw-dev \
+    libpulse-dev libnl-genl-3-dev feh notification-daemon dunst \
+    python3-sphinx python3-packaging libuv1-dev libcairo2-dev \
+    python3-xcbgen libxcb-ewmh-dev libjsoncpp-dev libmpdclient-dev \
+    libcurl4-openssl-dev xcb-proto policykit-1-gnome \
+    python3-gi gir1.2-gtk-3.0 python3-gi-cairo python3-cairo \
+    python3-setuptools python3-babel python3-dbus playerctl \
+    fonts-font-awesome slop gir1.2-ayatanaappindicator3-0.1 \
+    libgtk-4-dev libx11-dev libxcomposite-dev libxfixes-dev \
+    libgl1-mesa-dev libxi-dev libwayland-dev \
+    libncurses5-dev libreadline-dev usbview v4l-utils \
+    libxrender-dev libglew-dev python3-venv
+
+  # sudo aptitude install \
+  #     openjdk-8-jre=8u312-b07-0ubuntu1 \
+  #     openjdk-8-jre-headless=8u312-b07-0ubuntu1
   sudo updatedb
 }
 
