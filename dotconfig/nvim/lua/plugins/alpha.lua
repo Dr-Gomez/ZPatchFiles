89827e8da207054e70f51c1b56f1ad6f72cf65a6
Dr-Gomez <gomez.contact.mgz@gmail.com>
2024-08-28 15:13:58 +0100

    Added extra symbols to startup screen.

--- dotconfig/nvim/lua/plugins/alpha.lua
+++ dotconfig/nvim/lua/plugins/alpha.lua
@@ -19,10 +19,10 @@ return {
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("f", "󰆋  Find file", ":Telescope find_files <CR>"),
       dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
       dashboard.button("s", "  Settings", ":e $MYVIMRC <CR>"),
-      dashboard.button("q", "  Quit", ":qa<CR>"),
+      dashboard.button("q", "󰩈  Quit", ":qa<CR>"),
     }
 
     -- Set footer
